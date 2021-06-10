592a4a6b71ab8dce4d98e2fd597786f6a224f531
M-Spitfire <xcw97@outlook.com>
2021-06-10 16:38:59 +0800

    二叉树中序遍历

---
+++ Tree/897.递增顺序搜索树.java
@@ -0,0 +1,37 @@
+package Tree;
+/**
+ * leetcode_897
+ * 
+ * 简单的中序遍历, 只是空间可以再优化
+ */
+public class leetcode_897 {
+
+    TreeNode res = null;
+    TreeNode pre = null;
+    public TreeNode increasingBST(TreeNode root) {
+        if(root == null)return null;
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+        //遍历左子树
+        increasingBST(left);
+        //处理根节点
+        if(pre == null){
+            res = root;
+            pre = root;
+            res.left = null;
+            res.right = null;
+        }
+        else{
+            pre.right = root;
+            pre = root;
+            pre.left = null;
+            pre.right = null;
+        }
+        //遍历右子树
+        increasingBST(right);
+
+        return res;
+    }
+}
