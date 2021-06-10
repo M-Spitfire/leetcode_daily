39d475a16f3dc7432c5e2e6d8cde7b8d9f99ae22
M-Spitfire <xcw97@outlook.com>
2021-06-10 16:40:37 +0800

    二叉树中序遍历

---
+++ Tree/938.二叉搜索树的范围和.java
@@ -0,0 +1,25 @@
+package Tree;
+/**
+ * leetcode_938
+ * 
+ * 简单的中序遍历题
+ */
+public class leetcode_938 {
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null)return 0;
+
+        int res = rangeSumBST(root.left, low, high);
+        if(root.val > high)return res;
+        else if(root.val >= low) res += root.val;
+        res += rangeSumBST(root.right, low, high);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 构建二叉树稍微有点麻烦, 这里就不演示了
+         */
+    }
+}
