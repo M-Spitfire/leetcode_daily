00e7c587ef016393bd8ac52c85b4ffdaf71a32d0
M-Spitfire <xcw97@outlook.com>
2021-06-14 00:27:26 +0800

    简单的二分查找

---
+++ BinarySearch/374.猜数字大小.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=java
+ *
+ * [374] 猜数字大小
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution{
+    public int guessNumber(int n) {
+        int left = 1, right = n;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            int t = guess(mid);
+            if(t == 0)return mid;
+            else if(t == 1)left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+
+    // 防止报错的无意义的函数
+    public int guess(int n){
+        return 0;
+    }
+}
+// @lc code=end
+
