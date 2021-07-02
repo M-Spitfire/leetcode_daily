da7ef59c55d056d02a0e1193ac896081ac40797f
M-Spitfire <xcw97@outlook.com>
2021-07-02 08:42:39 +0800

    贪心问题, 与01背包非常相似, 注意区分

---
+++ 1833.雪糕的最大数量.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=1833 lang=java
+ *
+ * [1833] 雪糕的最大数量
+ * 贪心问题, 与背包问题非常相似, 但是本题选取的顺序可以是任意的, 因此用贪心做是更加简单明了的方式
+ */
+
+// @lc code=start
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int c = 0;
+        int res = 0;
+        for(int n : costs){
+            c += n;
+            if(c > coins){
+                return res;
+            }
+            else{
+                res++;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
