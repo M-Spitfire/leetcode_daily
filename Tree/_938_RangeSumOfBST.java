6095e4c6bc82e95fcc5b28d23f341f65abaecb8b
M-Spitfire <xcw97@outlook.com>
2021-08-20 15:28:04 +0800

    二叉树的中序遍历

--- Tree/_938_RangeSumOfBST.java
+++ Tree/_938_RangeSumOfBST.java
@@ -1,10 +1,9 @@
 package Tree;
 /**
- * leetcode_938
  * 
  * 简单的中序遍历题
  */
-class leetcode_938 {
+public class _938_RangeSumOfBST {
 
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null)return 0;
@@ -15,11 +14,5 @@ class leetcode_938 {
         res += rangeSumBST(root.right, low, high);
 
         return res;
-    }
-
-    public static void main(String[] args) {
-        /**
-         * 构建二叉树稍微有点麻烦, 这里就不演示了
-         */
     }
 }
