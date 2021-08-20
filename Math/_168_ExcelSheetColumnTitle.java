43376858d9c521cf09da172bb74d2b71bb706eff
M-Spitfire <xcw97@outlook.com>
2021-08-20 16:21:00 +0800

    实际上就是26进制, 注意这个26进制没有0

--- Math/_168_ExcelSheetColumnTitle.java
+++ Math/_168_ExcelSheetColumnTitle.java
@@ -5,10 +5,9 @@ package Math;
  * 但是要注意的是, 这个进制没有0, 因此在得到0的时候的处理相对其他数来说要特殊一些
  */
 
-// @lc code=start
-class Solution {
-    static char[] chs = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    public static String convertToTitle(int columnNumber) {
+public class _168_ExcelSheetColumnTitle {
+    char[] chs = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    public String convertToTitle(int columnNumber) {
         StringBuilder sb = new StringBuilder();
         while(columnNumber > 0){
             if(columnNumber % 26 == 0){
@@ -21,15 +20,9 @@ class Solution {
                 sb.insert(0, chs[index]);
                 columnNumber /= 26;
             }
-            
+
         }
         return sb.toString();
     }
-
-    public static void main(String[] args) {
-        String res = convertToTitle(701);
-        System.out.println(res);
-    }
 }
-// @lc code=end
 
