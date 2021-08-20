90928cab2151f5ae3328d0710b1ab9cc1cc3bbcc
M-Spitfire <xcw97@outlook.com>
2021-08-20 17:46:53 +0800

    两个容量限制的背包问题

--- DP/_879_ProfitableSchemes.java
+++ DP/_879_ProfitableSchemes.java
@@ -3,19 +3,18 @@ package DP;
 /**
  * 这题同样是背包问题, 但是容量限制有两个:人数和最小利润
  */
-class leetcode_879 {
-
-    public static int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+public class _879_ProfitableSchemes {
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
         int len = group.length;
         /**
          * dp[i][j][k]:
-         * 在前i个profit中选若干个个数, 
+         * 在前i个profit中选若干个个数,
          * 使它们的和大于等于k, 且每个profit对应的group的和小于等于j的方案的个数
-         * 
+         *
          * 初始化: dp[0][0][0] = 1
          * 显然, 当工作数量, 人数要求和最小利润要求都是0时, 不选择任何工作是完成目标的唯一方式
          * 但是若工作数量为0, 人数要求或者最小利润要求大于0时, 显然是不可能完成的
-         * 
+         *
          * 状态转移方程:
          * group[i]:完成当前任务需要的人数
          * j:当前一共拥有的人数
@@ -47,12 +46,5 @@ class leetcode_879 {
         }
 
         return res;
-    }
-
-    public static void main(String[] args) {
-        int[] group = {80,40};
-        int[] profit = {88,88};
-        int res = profitableSchemes(64, 0, group, profit);
-        System.out.println(res);
     }
 }
