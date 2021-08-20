473f568407a7809e47decdfe353ffcaee872aa11
M-Spitfire <xcw97@outlook.com>
2021-08-20 17:28:44 +0800

    删除数组中的指定元素

--- Array/_27_RemoveElement.java
+++ Array/_27_RemoveElement.java
@@ -6,7 +6,7 @@ package Array;
  * 删除数组中指定的元素
  */
 public class _27_RemoveElement {
-    public static int removeElement(int[] nums, int val) {
+    public int removeElement(int[] nums, int val) {
         int index = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != val){
