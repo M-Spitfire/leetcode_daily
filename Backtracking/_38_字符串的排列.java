3fc68c6cfc2c0ee16931b53a7403ca0ffd465c76
M-Spitfire <xcw97@outlook.com>
2021-08-21 16:22:03 +0800

    字母的全排列，显然是回溯法。

--- Backtracking/_38_字符串的排列.java
+++ Backtracking/_38_字符串的排列.java
@@ -1,5 +1,7 @@
 package Backtracking;
 
+import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,9 +11,8 @@ import java.util.Set;
  * 回溯+set去重
  * 效率不高，以后再来优化
  */
-class Solution {
-
-    public static String[] permutation(String s) {
+public class _38_字符串的排列 {
+    public String[] permutation(String s) {
         boolean[] visit = new boolean[s.length()];
         Set<String> set = new HashSet<>();
         build(s, visit, new StringBuilder(), set);
@@ -23,7 +24,7 @@ class Solution {
         return res;
     }
 
-    public static void build(String s, boolean[] visit, StringBuilder sBuilder, Set<String> res){
+    public void build(String s, boolean[] visit, StringBuilder sBuilder, Set<String> res){
         boolean flag = true;
         for(int i = 0; i < visit.length; i++){
             if(!visit[i]){
@@ -37,13 +38,6 @@ class Solution {
         }
         if(flag){
             res.add(sBuilder.toString());
-        }
-    }
-
-    public static void main(String[] args) {
-        String[] res = permutation("aba");
-        for(String str : res){
-            System.out.println(str);
         }
     }
 }
